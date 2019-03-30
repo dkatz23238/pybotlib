0410d5615446cac009f15ad8520382d2994faac5
David Emmanuel Katz <37519472+dkatz23238@users.noreply.github.com>
2019-03-30 20:23:33 -0300

    Update get_geckodriver.sh

--- get_geckodriver.sh
+++ get_geckodriver.sh
@@ -1,3 +1,3 @@
-wget https://github.com/mozilla/geckodriver/releases/download/v0.23.0/geckodriver-v0.23.0-linux64.tar.gz
+wget -nv https://github.com/mozilla/geckodriver/releases/download/v0.23.0/geckodriver-v0.23.0-linux64.tar.gz
 tar xvfz geckodriver-v0.23.0-linux64.tar.gz
 yes | rm -r geckodriver-v0.23.0-linux64.tar.gz
