c95b129792c1092246a9e62077e1735bffd8b01e
David <davidemmanuelkatz@gmail.com>
2019-04-20 18:02:50 -0300

    mods to python37

--- env-vars.sh
+++ env-vars.sh
@@ -1,7 +1,7 @@
 # Minio variables
-export MINIO_URL=minio.crossentropy.solutions;
-export MINIO_ACCESS_KEY=fa592f5a8eb34a46bb6789ca6ee4be52;
-export MINIO_SECRET_KEY=0ec6095ed0ed431cb2397650affcfa80;
+export MINIO_URL=localhost;
+export MINIO_ACCESS_KEY=AKIAIOSFODNN7EXAMPLE;
+export MINIO_SECRET_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY;
 export MINIO_OUTPUT_BUCKET_NAME=investigator-rpa-output;
 # Google Sheets Document ID
 export GSHEET_ID=1pBecz5Db9eK0QDR_oePmamdaFtEiCaO69RaE-Ozduko;
