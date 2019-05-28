97676562966b5216b9ea4c9939899fbb6ad9b6d3
David Emmanuel Katz <37519472+dkatz23238@users.noreply.github.com>
2019-05-28 17:08:36 -0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ author = 'David Katz'
 # The full version, including alpha/beta/rc tags
 release = '0.0.1'
 
-
+master_doc = 'index'
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
